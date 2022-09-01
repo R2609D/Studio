7369a928d45f074a33058d9e2a7f3afd623276b1
Anto200533 <antojohnmathews@gmail.com>
2022-09-01 17:24:24 +1000

    Creation of Functions

--- Src/web_scraper.py
+++ Src/web_scraper.py
@@ -10,16 +10,46 @@ from bs4 import BeautifulSoup
 from movielist import movie_name
 import requests
 
-for i in range(len(movie_name)):
-    api_request = requests.get('https://imsdb.com/scripts/{}.html'.format(movie_name[i])).text
-    soup = BeautifulSoup(api_request, 'lxml')
-    script = soup.find('pre')
-    output = script.text
-
-
-    with open('scripts.txt', 'a') as f:
-        f.write(output)
-
-#TODO: Convert this into a functions with try & expect error catching
 #TODO: Add a way selecting different genres for the script.txt dataset
+
+#***********************************************************************************************************
+#   
+#   This function scrapes the Imsdb script database for the movies in the list & outputs just the script 
+#
+#***********************************************************************************************************
+
+def get_script(): 
+    try: 
+        for i in range(len(movie_name)):
+            api_request = requests.get('https://imsdb.com/scripts/{}.html'.format(movie_name[i])).text
+            soup = BeautifulSoup(api_request, 'lxml')
+            script = soup.find('pre')
+            output = script.text
+            return output
+    except Exception as e: 
+        raise Exception(str(e))
+
+#***********************************************************************************************************
+#   
+#   This function appends all the scripts and store it in the scripts.txt file 
 #TODO: Fix the path to where script.txt file is created 
+#***********************************************************************************************************
+
+
+def store_scripts(file):
+    try:
+        with open('scripts.txt', 'a') as f:
+            f.write(file)          
+    except Exception as e: 
+        raise Exception(str(e))
+
+
+#***********************************************************************************************************
+#   
+#   Move this to main.py later 
+#
+#***********************************************************************************************************
+
+data = get_script()
+combined_data = store_scripts(data)
+
