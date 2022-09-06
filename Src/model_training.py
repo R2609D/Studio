922af9d20a1e23e9a382cc2a76b7a2cffd23c3b1
lllandthe <13933864@student.uts.edu.au>
2022-09-06 11:46:56 +1000

    test

--- Src/model_training.py
+++ Src/model_training.py
@@ -1,10 +1,10 @@
-#***********************************************************************************************************
+# ***********************************************************************************************************
 # Summary: Creation of the Model and Data preprocessing
-#          
+#
 # Requirements: Python Verison 3.7
 #
-# Author : Anto 
-#***********************************************************************************************************
+# Author : Anto
+# ***********************************************************************************************************
 import tensorflow as tf
 import numpy as np
 import os
@@ -13,50 +13,52 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, LSTM, Dropout
 from string import punctuation
 
-#***********************************************************************************************************
-# 
-# Global Variables 
+# ***********************************************************************************************************
 #
-#***********************************************************************************************************
+# Global Variables
+#
+# ***********************************************************************************************************
 
 sequence_length = 100
 BATCH_SIZE = 128
 EPOCHS = 30
 
-#***********************************************************************************************************
-# 
-#  fill in later 
-# 
-#***********************************************************************************************************
+# ***********************************************************************************************************
+#
+#  fill in later
+#
+# ***********************************************************************************************************
 
-def get_path(path): 
-    try: 
+
+def get_path(path):
+    try:
         path = 'Src/scripts.txt'
         basename = os.path.basename(path)
-        text = open(path,encoding="utf-8").read()
+        text = open(path, encoding="utf-8").read()
         text = text.lower()
         return text
-    except Exception as e: 
+    except Exception as e:
         raise Exception(str(e))
 
-#***********************************************************************************************************
-# 
-#  fill in later 
-# 
-#***********************************************************************************************************
+# ***********************************************************************************************************
+#
+#  fill in later
+#
+# ***********************************************************************************************************
 
-def data_stats(text): 
+
+def data_stats(text):
     try:
         n_char = len(text)
         unique_char = ''.join(sorted(set(text)))
         no_of_characters = ("No of Characater:", n_char)
-        no_of_uniq_characters = ("No Of Unique Characters:",unique_char)
-        return no_of_characters,no_of_uniq_characters
-        
+        no_of_uniq_characters = ("No Of Unique Characters:", unique_char)
+        return no_of_characters, no_of_uniq_characters
+
     except Exception as e:
         raise Exception(str(e))
 
-def conv_2_int(): 
-    return 
 
-
+def conv_2_int():
+    # jfssdnfdjfs
+    return
