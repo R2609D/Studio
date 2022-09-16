7c9e13ae85f16814c4a6d209281c2fdffe7604fb
R2609D <ryanduong2609@gmail.com>
2022-09-16 16:43:41 +1000

    Update movielist.py

--- Src/movielist.py
+++ Src/movielist.py
@@ -6,7 +6,14 @@
 # Author : Anto 
 #***********************************************************************************************************
 
-movie_name = ['Blade','Alien','Die-Hard-2']
+movie_name = ['Blade','Alien','Die-Hard-2','Alien-3','Aliens','Alone-in-the-Dark','American-Outlaws',
+              'American-Sniper','Apocalypse-Now','Armageddon','Assassins','Avatar','Backdraft','Batman',
+               'Braveheart','Breakdown','Catwoman','Cliffhanger','Commando','Crank','Deadpool','Drive',
+               'Dune','Equilibrium','Face-Off','Feast','Fury','Gamer','Ghost-Rider', 'Ghost-Ship','Godzilla',
+               'Hancock','Heat','Inception','Innerspace','Jade','King-Kong','Legion','Logan','Looper','Machete',
+               'Metro','Next','Outbreak','Pearl-Harbor','Platoon','Pulp-Fiction','Ronin','Serenity','Soldier',
+               'Spider-Man','Terminator','Thor','Ticker','TMNT','Top-Gun','TRON','Virtuosity','Wanted','Warrior',
+               'Whiteout','Wild-Hogs','Willow','Wonder-Woman','Sneakers','Rush','Robocop','Resident-Evil','Next']
 
 
 #TODO: Add a fuck ton of movies for Action Genre 
