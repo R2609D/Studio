4628f095d1577cabecae750799709aa7863f1bac
R2609D <ryanduong2609@gmail.com>
2022-09-19 22:27:50 +1000

    Update movielist.py

--- Src/movielist.py
+++ Src/movielist.py
@@ -6,7 +6,7 @@
 # Author : Anto 
 #***********************************************************************************************************
 
-movie_name = ['Blade','Alien','Die-Hard-2','Alien-3','Aliens','Alone-in-the-Dark','American-Outlaws',
+action_name = ['Blade','Alien','Die-Hard-2','Alien-3','Aliens','Alone-in-the-Dark','American-Outlaws',
               'American-Sniper','Apocalypse-Now','Armageddon','Assassins','Avatar','Backdraft','Batman',
                'Braveheart','Breakdown','Catwoman','Cliffhanger','Commando','Crank','Deadpool','Drive',
                'Dune','Equilibrium','Face-Off','Feast','Fury','Gamer','Ghost-Rider', 'Ghost-Ship','Godzilla',
@@ -15,6 +15,18 @@ movie_name = ['Blade','Alien','Die-Hard-2','Alien-3','Aliens','Alone-in-the-Dark
                'Spider-Man','Terminator','Thor','Ticker','TMNT','Top-Gun','TRON','Virtuosity','Wanted','Warrior',
                'Whiteout','Wild-Hogs','Willow','Wonder-Woman','Sneakers','Rush','Robocop','Resident-Evil','Next']
 
+comedy_name = ['Airplane','Alladin','American-Pie','Annie-Hall','Arthur','Bad-Boys','Bean','Big','Birdman',
+                'Brazil','Bridesmaids','Bruce-Almighty','Cable-Guy','Charade','Cherry-Falls','Clerks','Coco',
+                'Dark-Star','Date-Night','Diner','Drop-Dead-Gorgeous','Ed-TV','Entrapment','Extract','Feast',
+                'Fletch','Freaked','Funny-People','Get-Shorty','Go','Gremlins','Hancock','Happy-Feet',
+                'In-The-Loop','Jackie-Brown','Juno','Knocked-Up','Life','Made','Men-In-Black','Next-Friday'
+                'Nurse-Betty','Office-Space','Paul','Petty-Woman','Real-Genius','Rush-Hour','Semi-Pro',
+                'Sex-and-the-City','Shrek','Smashed','Stepmom','Storytelling','Ted','Tin-Cup','Up','Zootopia']
 
+horror_name = ['A-Quiet-Place','Bad-Dreams','Bones','Blade','Carrie','Copycat','Cube','Deep-Rising','Evil-Dead',
+                'Feast','Friday-the-13th','Friday-Night','From-Dusk-Till-Dawn','Get-Out','Gremlins','Hannibal',
+                'Heavy-Metal','Hellboy','Hellraiser','Hollow-Man','It','Jaws','Lake-Placid','Lost-Highway',
+                'Leviathan','Mimic','Mirrors','Misery','Orphan','Priest','Psycho','Ringu','Saw','Signs','Slither',
+                'Suspect-Zero','They','Tremors','Wind-Chill','Twin-Peaks']
 #TODO: Add a fuck ton of movies for Action Genre 
 #TODO: Create another list of movies for a different genre
